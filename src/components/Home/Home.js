dff9d4f1a76252a99a21962ba8e785551c118a0f
Sjon de Gast <sjondegast@gmail.com>
2019-12-11 21:27:49 +0100

    added fetch method to home.js and imported api url, key from config.js and set the strate to take those values tested api with console.log and it works

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,15 @@
 import React, { Component } from "react";
+import {
+  API_URL,
+  API_KEY,
+  // eslint-disable-next-line
+  IMAGE_BASE_URL,
+  // eslint-disable-next-line
+  POSTER_SIZE,
+  // eslint-disable-next-line
+  BACKDROP_SIZE
+} from "../../config";
+
 import HeroImage from "../elements/HeroImage/HeroImage";
 import SearchBar from "../elements/SearchBar/SearchBar";
 import FourColGrid from "../elements/FourColGrid/FourColGrid";
@@ -9,7 +20,34 @@ import Spinner from "../elements/Spinner/Spinner";
 import "./Home.css";
 
 class Home extends Component {
-  state = {};
+  state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ""
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems(endpoint) {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        });
+      });
+  }
   render() {
     return (
       <div>
