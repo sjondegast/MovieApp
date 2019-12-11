8df77f73282e9e548a59b5f3742bdac51cd67074
Sjon de Gast <sjondegast@gmail.com>
2019-12-11 22:13:07 +0100

    loadMoreItems

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,6 +35,21 @@ class Home extends Component {
     this.fetchItems(endpoint);
   }
 
+  loadMoreItems = () => {
+    let endpoint = "";
+    this.setState({ loading: true });
+
+    if (this.state.searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie/?api_key=${API_KEY}&language=en-US&query=${
+        this.state.searchTerm
+      }&page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  };
+
   fetchItems(endpoint) {
     fetch(endpoint)
       .then(result => result.json())
