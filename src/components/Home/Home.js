a262450b0d9bf149e050d61352b1da4db74ab06c
Sjon de Gast <sjondegast@gmail.com>
2019-12-13 22:00:28 +0100

    added fourcolgrid component and added it to the render method in home.js and gave it the props it needs to render

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -93,7 +93,28 @@ class Home extends Component {
           </div>
         ) : null}
         <SearchBar callback={this.searchItems} />
-        <FourColGrid />
+        <div className="rmdb-home-grid">
+          <FourColGrid
+            header={this.state.searchTerm ? "Search Results" : "Popular Movies"}
+            loading={this.state.loading}
+          >
+            {this.state.movies.map((element, i) => {
+              return (
+                <MovieThumb
+                  key={i}
+                  clickable={true}
+                  image={
+                    element.poster_path
+                      ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`
+                      : "./images/no_image.jpg"
+                  }
+                  movieId={element.id}
+                  movieName={element.original_title}
+                />
+              );
+            })}
+          </FourColGrid>
+        </div>
         <Spinner />
         <LoadMoreBtn />
       </div>
