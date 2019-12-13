ad6854d975eb0839a7b2c0798fa2d657cff957f1
Sjon de Gast <sjondegast@gmail.com>
2019-12-13 22:32:37 +0100

    added browserRouter to main app App.je - build Movie Component Class and NotFoundPage

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Movie extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Movie Title Homepage</h1>
+      </div>
+    );
+  }
+}
+
+export default Movie;
